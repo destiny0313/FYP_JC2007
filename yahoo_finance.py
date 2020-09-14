05e6f7f89d053530f656ae5ff252d9027ff15929
leowong1120 <49244227+leowong1120@users.noreply.github.com>
2020-09-15 02:54:06 +0800

    Haven't debug

--- yahoo_finance.py
+++ yahoo_finance.py
@@ -16,14 +16,22 @@ sql_select_query = "select * from stock_list"
 mycursor.excute(sql_select_query)
 records = mycursor.fetchall()
 count = mycursor.rowcount
+
+sql = "INSERT INTO 'historical_price' ('company_id', 'filepath') VALUES (%s, %s)"
+
 int i = 0
 while i < count :
     for row in records :
         company_id = row[0]
-        url = ("https://query1.finance.yahoo.com/v7/finance/download/%s?period1=1441929600&period2=1599782400&interval=1d&events=history" %(company_id))
-        ur.urlretrieve(url, "%s.csv" %(company_id))
-        path = ("C:\python\%s.csv" %(company_id))
+        url = ("https://query1.finance.yahoo.com/v7/finance/download/" + company_id + "?period1=1441929600&period2=1599782400&interval=1d&events=history")
+        ur.urlretrieve(url, company_id + ".csv")
+        path = ("C:\python\" + company_id + ".csv")
+        mycursor.execute(sql, (company_id, path))
         i ++
 
+mydb.commit()
+finally:
+mycursor.close()
 
-driver = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe")
+
+
