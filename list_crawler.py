33f15f2b1f4b6765d97a992f5599615bdb03df85
Destiny <49312817+destiny0313@users.noreply.github.com>
2020-10-23 20:07:43 +0800

    modified:   list_crawler.py

--- list_crawler.py
+++ list_crawler.py
@@ -24,7 +24,7 @@ options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 options.add_argument('--no-sandbox')
                      
-driver = webdriver.Chrome(executable_path="/data/opt/users/destiny/chromedriver")
+driver = webdriver.Chrome(executable_path="/data/opt/users/destiny/chromedriver", chrome_options=options)
 driver.get("https://www.nyse.com/listings_directory/stock")
 
 
