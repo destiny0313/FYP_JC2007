968806fe05419ce03d7fa5375827d25b8f9f9488
Destiny <49312817+destiny0313@users.noreply.github.com>
2020-10-25 03:16:37 +0800

    modified:   yahoo_finance.py

--- yahoo_finance.py
+++ yahoo_finance.py
@@ -25,8 +25,7 @@ for row in records :
         ur.urlretrieve(url, "/data/opt/users/destiny/Historical_Price/" + company_id + ".csv")
 
     except HTTPError as error:
-        print(error+ " - "+company_id);
-        mycursor.execute(sql, (company_id, "No data available"))
+        print(error+ " - "+company_id)
     mydb.commit()
     count = count + 1
     if count%1000 == 0:
