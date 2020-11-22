2fe779856c492a9c0e0df364d8788589a642858b
Destiny <49312817+destiny0313@users.noreply.github.com>
2020-11-23 01:22:53 +0800

    modified:   Analysis_Module/fundamental_ranking.py

--- Analysis_Module/fundamental_ranking.py
+++ Analysis_Module/fundamental_ranking.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import csv
+import numpy as np
 
 
 tmp_df = pd.DataFrame(columns=["Country","Sector","Industry","Net_Income_Ratio","Operating_Ratio_Income",
@@ -39,15 +40,15 @@ with open("/data/opt/users/destiny/resource/Stock_List.csv") as stocklist:
             filename = "/data/opt/users/destiny/resource/income_statement_annual/"+row[0]+"_IncomeStatement.csv"
             with open(filename) as in_s:
                 in_s_r = [row for row in csv.reader(in_s)]
-                Net_Income_Ratio = in_s_r[25][1]
-                Operating_Ratio_Income = in_s_r[19][1]
-                Gross_Profit_Ratio = in_s_r[7][1]
-                EPS = in_s_r[26][1]
+                Net_Income_Ratio = float(in_s_r[25][1])
+                Operating_Ratio_Income = float(in_s_r[19][1])
+                Gross_Profit_Ratio = float(in_s_r[7][1])
+                EPS = float(in_s_r[26][1])
         except IOError:
-            Net_Income_Ratio = "NaN"
-            Operating_Ratio_Income = "NaN"
-            Gross_Profit_Ratio = "NaN"
-            EPS = "NaN"
+            Net_Income_Ratio = np.NaN
+            Operating_Ratio_Income = np.NaN
+            Gross_Profit_Ratio = np.NaN
+            EPS = np.NaN
         
         
         ######################################################  
@@ -58,25 +59,25 @@ with open("/data/opt/users/destiny/resource/Stock_List.csv") as stocklist:
             with open(filename) as km:
             #with open("C:\\Users\\desti\\Documents\\GitHub\\FYP_JC2007\\Example\\KeyMetric.csv") as km:
                 km_r = [row for row in csv.reader(km)]
-                Working_Capital = km_r[43][1]
-                ROE = km_r[56][1]
-                PE_Ratio = km_r[12][1]
-                PB_Ratio = km_r[16][1]
-                Current_Ratio = km_r[27][1]
-                Debt_To_Equity = km_r[16][1]
-                Debt_To_Asset = km_r[25][1]
-                Dividend_Yield = km_r[30][1]
-                Market_Capital = km_r[10][1]
+                Working_Capital = float(km_r[43][1])
+                ROE = float(km_r[56][1])
+                PE_Ratio = float(km_r[12][1])
+                PB_Ratio = float(km_r[16][1])
+                Current_Ratio = float(km_r[27][1])
+                Debt_To_Equity = float(km_r[16][1])
+                Debt_To_Asset = float(km_r[25][1])
+                Dividend_Yield = float(km_r[30][1])
+                Market_Capital = float(km_r[10][1])
         except IOError:
-            Working_Capital = "NaN"
-            ROE = "NaN"
-            PE_Ratio = "NaN"
-            PB_Ratio = "NaN"
-            Current_Ratio = "NaN"
-            Debt_To_Equity = "NaN"
-            Debt_To_Asset = "NaN"
-            Dividend_Yield = "NaN"
-            Market_Capital = "NaN"
+            Working_Capital = np.NaN
+            ROE = np.NaN
+            PE_Ratio = np.NaN
+            PB_Ratio = np.NaN
+            Current_Ratio = np.NaN
+            Debt_To_Equity = np.NaN
+            Debt_To_Asset = np.NaN
+            Dividend_Yield = np.NaN
+            Market_Capital = np.NaN
         
 
     ######################################################  
@@ -91,4 +92,7 @@ with open("/data/opt/users/destiny/resource/Stock_List.csv") as stocklist:
                                   "Dividend_Yield":Dividend_Yield,"Market_Capital":Market_Capital}, index=[stockname])
 
         tmp_df = tmp_df.append(stockname)
-    print(tmp_df)
+    
+    df_a = tmp_df.sort_values(Working_Capital, na_position="last", ascending=True)
+    list_a = list(df_a.index.values)
+    print(list_a)
