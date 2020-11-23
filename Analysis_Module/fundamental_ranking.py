a7f67c14dc7f36074bf90237a008476ebe23cdf2
Destiny <49312817+destiny0313@users.noreply.github.com>
2020-11-23 12:54:56 +0800

    modified:   Analysis_Module/fundamental_ranking.py

--- Analysis_Module/fundamental_ranking.py
+++ Analysis_Module/fundamental_ranking.py
@@ -171,4 +171,4 @@ with open("/data/opt/users/destiny/resource/Stock_List.csv") as stocklist:
     
     df_a = tmp_df.sort_values(by="Dividend_Yield", na_position="last", ascending=True)
     list_a = list(df_a.index.values)
-    print(list_a)
+    print(df_a)
