679c847c443a1bb3450f5386d8eee459b6b61920
destiny0313 <destinyyau36@gmail.com>
2020-11-26 16:51:58 +0800

    modified:   Analysis_Module/fundamental_similarity_search.py

--- Analysis_Module/fundamental_similarity_search.py
+++ Analysis_Module/fundamental_similarity_search.py
@@ -1,3 +1,12 @@
 import falconn as fal
+import numpy as np
+import pandas as pd
+import os
 
 x = fal.LSHIndex(fal.get_default_parameters)
+filepath = os.path.abspath(os.getcwd())+"//..//Source//attribute_ranking.csv"
+with open(filepath) as ranking:
+    df_tmp = pd.read_csv(ranking)
+    df = pd.DataFrame.to_numpy(df_tmp)
+    
+x.setup(df)
