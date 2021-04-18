f320d63135eb4cf7454796f4299987c34b7b025b
destiny0313 <49312817+destiny0313@users.noreply.github.com>
2021-04-18 12:30:12 +0800

    modified:   Analysis_Module/fundamental_similarity_search.py

--- Analysis_Module/fundamental_similarity_search.py
+++ Analysis_Module/fundamental_similarity_search.py
@@ -1,16 +1,31 @@
-import falconn as fal
-import numpy as np
+from lshashpy3 import LSHash
 import pandas as pd
 import os
 
-x = fal.get_default_parameters(6685,26)
-y = fal.LSHIndex(x)
 filepath = os.path.abspath(os.getcwd())+"//..//Source//attribute_ranking.csv"
+
+lsh = LSHash(6,13)
+
 with open(filepath) as ranking:
-    df_tmp = pd.read_csv(ranking)
+    df = pd.read_csv(ranking)
+    df.fillna(0, inplace = True)
     
-df_tmp = df_tmp.drop(['Unnamed: 0', 'Country','Sector','Industry'], axis=1)
-df = pd.DataFrame.to_numpy(df_tmp)
-print(df_tmp)
-    
-y.setup(df)
+    print(df)
+    i = 0
+    for index,row in df.iterrows():
+        lsh.index([row["Net_Income_Ratio"], row["Operating_Income_Ratio"], row["Gross_Profit_Ratio"],
+                   row["EPS"], row["Working_Capital"], row["ROE"], row["PE_Ratio"], row["PB_Ratio"],
+                   row["Current_Ratio"], row["Debt_To_Equity"], row["Debt_To_Asset"], row["Dividend_Yield"],
+                   row["Market_Capital"]], extra_data = row["Unnamed: 0"])
+    query = "TSLA"
+    target = []
+    for index,row in df.iterrows():
+        if row["Unnamed: 0"] == query:
+            target.extend([row["Net_Income_Ratio"], row["Operating_Income_Ratio"], row["Gross_Profit_Ratio"],
+                   row["EPS"], row["Working_Capital"], row["ROE"], row["PE_Ratio"], row["PB_Ratio"],
+                   row["Current_Ratio"], row["Debt_To_Equity"], row["Debt_To_Asset"], row["Dividend_Yield"],
+                   row["Market_Capital"]])
+    top_n = 10
+    result = lsh.query(target, num_results=top_n, distance_func="euclidean")
+    for((vec,extra_data),distance) in result:
+        print(extra_data)
